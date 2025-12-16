811facca177132189d9936ae24676e7e767ff81e
d9ya <diya26162@gmail.com>
2025-12-16 14:56:09 +0545

    commit

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import{BrowserRouter as Router, Routes,Route} from"react-router-dom";
 import Registerpage from"./pages/Registerpage";
 
+
+
 function App() {
  
   return (
 <Router>
   <Routes>
     <Route path="/Register" element={<Registerpage/>} />``
+   
   </Routes>
 </Router>
 
