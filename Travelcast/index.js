fbb8c06e78e67142c7e083772ed87893019d83d6
d9ya <diya26162@gmail.com>
2026-01-02 20:58:01 +0545

    Add initial Travelcast backend/frontend setup

--- Travelcast/index.js
+++ Travelcast/index.js
@@ -1,8 +1,22 @@
-const express = require("express")
-const app =express();
-app.get("/",(req,res)=>{
-    res.json({message:"welcome to the home page"});
+const express = require("express");
+const app = express();
+const { sequelize, connectDB } = require("./database/database");
+
+app.use(express.json());
+
+app.use("/api/user", require("./routes/auth.routes"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Homepage" });
 });
-app.listen(300,()=>{
-    console.log("server running on https://localhost.3000");
-});
+
+const startServer = async () => {
+  await connectDB();
+  await sequelize.sync();
+
+  app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+  });
+};
+
+startServer();
