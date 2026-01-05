ccf3ec4b5dd040be75a0165995610e7ad68b372d
bnsasht <binishasht.dev@gmail.com>
2026-01-05 21:00:22 +0545

    packing page frontend added

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import{BrowserRouter as Router, Routes,Route} from"react-router-dom";
 import Registerpage from"./pages/Registerpage";
 import LoginPage from "./pages/Loginpage";
 import Securitypage from"./pages/Securitypage";
+import Packing from "./pages/Packing";
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
     <Route path="/Register" element={<Registerpage/>} />``
         <Route path="/Login" element={<LoginPage/>} />``
         <Route path="/Security" element={<Securitypage/>} />``
+        <Route path="/Packing" element={<Packing/>} />``
   </Routes>
 </Router>
 
