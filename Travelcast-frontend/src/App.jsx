e6770c3dd9fcd32f16256d8c7353cab112998057
Dilashma27 <jharashmi3027@gmail.com>
2026-01-06 18:20:50 +0545

    Add UserDashboard page from development branch

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import{BrowserRouter as Router, Routes,Route} from"react-router-dom";
-import Registerpage from"./pages/Registerpage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registerpage from "./pages/Registerpage";
 import LoginPage from "./pages/Loginpage";
+import Securitypage from "./pages/Securitypage";
+import ForgotPasswordPage from "./forgotPasswordPage";
+import UserDashboard from "./pages/Userdashboard";
+
+
 
 function App() {
- 
   return (
-<Router>
-  <Routes>
-    <Route path="/Register" element={<Registerpage/>} />``
-        <Route path="/Login" element={<LoginPage/>} />``
-  </Routes>
-</Router>
-
-  )
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<Registerpage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/security" element={<Securitypage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/userdashboard" element={<UserDashboard/>} />
+      </Routes>
+    </Router>
+  );
 }
- 
-export default App;
 
- 
+export default App;
