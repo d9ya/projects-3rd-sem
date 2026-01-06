2bf6a0451e3fbd5943a7b95c2cb35cf77fcead85
Dilashma27 <jharashmi3027@gmail.com>
2026-01-06 13:09:51 +0545

    userdashboard

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Registerpage from "./pages/Registerpage";
 import LoginPage from "./pages/Loginpage";
 import Securitypage from "./pages/Securitypage";
 import ForgotPasswordPage from "./forgotPasswordPage";
+import UserDashboard from "./pages/Userdashboard";
+
+
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/security" element={<Securitypage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/userdashboard" element={<UserDashboard/>} />
       </Routes>
     </Router>
   );
