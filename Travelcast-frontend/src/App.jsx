23df6b0ba4f67a43f0c2446a453d3bb6e241c327
bnsasht <binishasht.dev@gmail.com>
2026-01-20 09:54:51 +0545

    Resolve merge conflicts in App.jsx

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -1,25 +1,20 @@
-import{BrowserRouter as Router, Routes,Route} from"react-router-dom";
-import Registerpage from"./pages/Registerpage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registerpage from "./pages/Registerpage";
 import LoginPage from "./pages/Loginpage";
-import Securitypage from"./pages/Securitypage";
+import Securitypage from "./pages/Securitypage";
 import Packing from "./pages/Packing";
 
 function App() {
- 
   return (
-<Router>
-  <Routes>
-    <Route path="/Register" element={<Registerpage/>} />``
-        <Route path="/Login" element={<LoginPage/>} />``
-        <Route path="/Security" element={<Securitypage/>} />``
-        <Route path="/Packing" element={<Packing/>} />``
-  </Routes>
-</Router>
-
-  )
-
+    <Router>
+      <Routes>
+        <Route path="/Register" element={<Registerpage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Security" element={<Securitypage />} />
+        <Route path="/Packing" element={<Packing />} />
+      </Routes>
+    </Router>
+  );
 }
- 
-export default App;
 
- 
+export default App;
