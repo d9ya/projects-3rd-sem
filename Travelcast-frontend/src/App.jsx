576afd091cdb1d5f13beb93ddf7a1ad2d3e1a77e
d9ya <diya26162@gmail.com>
2026-01-22 07:38:13 +0545

    Resolved merge conflict in App.jsx

--- Travelcast-frontend/src/App.jsx
+++ Travelcast-frontend/src/App.jsx
@@ -1,44 +1,31 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Registerpage from "./pages/Registerpage";
 import LoginPage from "./pages/Loginpage";
 import CreateTripPage from "./pages/CreateTrippage";
-import { Toaster } from "react-hot-toast";
+import Securitypage from "./pages/Securitypage";
+import ForgotPasswordPage from "./forgotPasswordPage";
+import UserDashboard from "./pages/Userdashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Toaster />
       <Routes>
-        <Route path="/Register" element={<Registerpage />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/createTrip" element={<CreateTripPage />} />
-      </Routes>
-    </BrowserRouter>
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Registerpage from "./pages/Registerpage";
-import LoginPage from "./pages/Loginpage";
-import Securitypage from "./pages/Securitypage";
-import ForgotPasswordPage from "./forgotPasswordPage";
-import UserDashboard from "./pages/Userdashboard";
-
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
+        {/* Auth */}
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<Registerpage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Registerpage />} />
         <Route path="/security" element={<Securitypage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/userdashboard" element={<UserDashboard/>} />
+
+        {/* App */}
+        <Route path="/createTrip" element={<CreateTripPage />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
       </Routes>
-    </Router>
->>>>>>> d8ca73670ebb72293bb1daf96e75a28864e21458
+    </BrowserRouter>
   );
 }
 
